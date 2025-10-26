588bfd4dcdbcbaac5118c21df2d011a741eb13c1
Mohamed Nasr <legendnasr@gmail.com>
2025-10-26 17:49:16 +0300

    orderItem isPayed

--- Migrations/20251026144314_OrderItemIsPayed.Designer.cs
+++ Migrations/20251026144314_OrderItemIsPayed.Designer.cs
@@ -4,6 +4,7 @@ using GradDemo.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GradDemo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251026144314_OrderItemIsPayed")]
+    partial class OrderItemIsPayed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20251026144314_OrderItemIsPayed.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GradDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderItemIsPayed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPayed",
+                table: "OrderItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+       
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPayed",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
